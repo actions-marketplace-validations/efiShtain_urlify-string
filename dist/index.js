d96c29b5ad5a2177cfe38b0b7e25a26668581fcc
Efi Shtain <efi.shtain@gmail.com>
2023-05-13 10:36:43 +0300

    update dist

--- dist/index.js
+++ dist/index.js
@@ -17,8 +17,9 @@ module.exports = (str) =>
   str
     .toLowerCase()
     .replace(/[^a-z0-9]/g, "-")
-    .slice(0, 62)
-    .replace(/(\A-+|-+\z)/g, "");
+    .replace(/^-+|-+$/g, "")
+    .slice(0, 63)
+    .trim();
 
 
 /***/ }),
